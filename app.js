dca883bf67aef201f03ada27a2b6266067fe2d33
Nergiz Şengün <nergizduygu56@gmail.com>
2025-02-07 22:40:07 +0300

    music player

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+const container = document.querySelector(".container");
+const image = document.querySelector("#music-image");
+const title = document.querySelector("#music-details .title");
+const singer = document.querySelector("#music-details .singer");
+const prev = document.querySelector("#controls #prev");
+const play = document.querySelector("#controls #play");
+const next = document.querySelector("#controls #next");
+
+
+
+
+
 const player = new MusicPlayer(musicList);
+
+window.addEventListener("load", () => {
+    let music = player.getMusic();
+    displayMusic(music);
+});
+
+function displayMusic(music){
+    title.innerText = music.title;
+    singer.innerText = music.singer;
+    image.src = "img/" + music.img;
+    audio.src = "mp3/" + music.file;
+
+}
+
